981e1a9f82a968d0e60933a07a46789ef06ab164
Jonathan Prieto <jonathan.prieto@me.com>
2019-08-31 14:30:30 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/flip-sort-10327-jonathanprieto99.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flip-sort-10327-jonathanprieto99.dir/main.cpp.o"
+  "flip-sort-10327-jonathanprieto99.pdb"
+  "flip-sort-10327-jonathanprieto99"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/flip-sort-10327-jonathanprieto99.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
